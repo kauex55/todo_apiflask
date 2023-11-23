3c966114dce32c88b8f40368c6040d8c7b45e575
kauex55 <kauelima2477@gmail.com>
2023-11-23 13:14:46 -0300

    up

--- script.js
+++ script.js
@@ -1,107 +1,64 @@
-class TodoManager {
-    constructor() {
-        this.taskTable = document.querySelector('.tabela-js');
-        this.addTaskForm = document.querySelector('.form-js');
-        this.deleteButton = document.getElementById('B-S');
-        this.editButton = document.getElementById('Editar');
-        this.selectedTaskId = 0;
-        this.editingTask = '';
+const tabela = document.querySelector('.tabela-js');
+const form = document.querySelector('.form-js');
+const BS = document.getElementById('B-S');
 
-        this.loadData();
-        this.setupEventListeners();
-    }
+axios.get('http://127.0.0.1:5000/list')
+    .then(response => exibirDados(response.data))
+    .catch(erro => console.log(erro));
 
-    loadData() {
-        axios.get('http://127.0.0.1:5000/list')
-            .then(response => this.displayTasks(response.data))
-            .catch(error => console.log(error));
-    }
-
-    displayTasks(tasks) {
-        this.taskTable.innerHTML = '';
-        tasks.forEach(task => {
-            this.taskTable.innerHTML += this.createTaskTableRow(task);
-        });
-    }
-
-    createTaskTableRow(task) {
-        return `
+function exibirDados(dados) {
+    tabela.innerHTML = ''; 
+    dados.forEach(item => {
+        tabela.innerHTML += `
             <tr>
-                <th scope="row">${task.ID}</th>
-                <td>${task.TAREFA}</td>
+                <th scope="row">${item.ID}</th>
+                <td>${item.TAREFA}</td>
                 <td>
                     <button class="btn" type="button" data-bs-toggle="modal"
-                        data-bs-target="#modalexcluir" onclick="app.selectTaskForDeletion(${task.ID})">
+                        data-bs-target="#modalexcluir" onclick="excluirTarefa(${item.ID})">
                         <span class="material-symbols-outlined text-danger">delete</span>
                     </button>
                     <button class="btn" type="button" data-bs-toggle="modal"
-                        data-bs-target="#modaleditar" onclick="app.selectTaskForEditing('${task.TAREFA}')">
+                        data-bs-target="#modaleditar" onclick="editarTarefa('${item.TAREFA}')">
                         <span class="material-symbols-outlined text-success">edit</span>
                     </button>
                 </td>
             </tr>
         `;
-    }
-
-    addTask() {
-        const taskInput = document.getElementById("Tarefa1");
-        const newTask = taskInput.value.trim();
-        if (newTask !== "") {
-            const json = { Tarefa: newTask };
-            axios.post('http://127.0.0.1:5000/add', json)
-                .then(response => {
-                    console.log("Tarefa adicionada com sucesso", response.data.Tarefa);
-                    this.addTaskForm.reset();
-                    this.loadData();
-                })
-                .catch(error => console.error("Ocorreu um erro ao adicionar", error));
-        }
-    }
-
-    selectTaskForDeletion(taskId) {
-        this.selectedTaskId = taskId;
-    }
-
-    confirmDeletion() {
-        const url = `http://127.0.0.1:5000/delete/${this.selectedTaskId}`;
-        axios.delete(url)
-            .then(response => {
-                console.log("Tarefa excluída com sucesso", response.data);
-                this.loadData();
-            })
-            .catch(error => console.error("Ocorreu um erro ao excluir", error));
-    }
-
-    selectTaskForEditing(task) {
-        this.editingTask = task;
-    }
-
-    confirmEditing() {
-        const editedTaskInput = document.getElementById("TarefaN");
-        const editedTask = editedTaskInput.value;
-        const url = `http://127.0.0.1:5000/update/${this.editingTask}/${editedTask}`;
-        axios.put(url)
-            .then(response => {
-                console.log("Tarefa atualizada com sucesso", response.data);
-                this.loadData();
-            })
-            .catch(error => console.error("Ocorreu um erro ao atualizar", error));
-    }
-
-    setupEventListeners() {
-        this.addTaskForm.addEventListener('submit', (event) => {
-            event.preventDefault();
-            this.addTask();
-        });
-
-        this.deleteButton.addEventListener('click', () => {
-            this.confirmDeletion();
-        });
-
-        this.editButton.addEventListener('click', () => {
-            this.confirmEditing();
-        });
-    }
+    });
 }
 
-const app = new TodoManager();
+form.addEventListener('submit', async function (evento) {
+    evento.preventDefault();
+    const tarefa = document.getElementById("Tarefa1").value;
+    if (tarefa.trim() !== "") {
+        try {
+            const resposta = await axios.post('http://127.0.0.1:5000/adicionar', { Tarefa: tarefa });
+            console.log("Tarefa adicionada com sucesso:", resposta.data.Tarefa);
+            form.reset(); 
+        } catch (erro) {
+            console.error("Erro ao adicionar a tarefa:", erro);
+        }
+    }
+});
+
+function excluirTarefa(id) {
+    const url = `http://127.0.0.1:5000/excluir/${id}`;
+    axios.delete(url)
+        .then(resposta => console.log("Tarefa excluída com sucesso:", resposta.data))
+        .catch(erro => console.error("Erro ao excluir a tarefa:", erro));
+}
+
+function editarTarefa(id, tarefaAtual) {
+    const novaTarefa = prompt("Editar tarefa:", tarefaAtual);
+    
+    if (novaTarefa !== null) {
+        const url = `http://127.0.0.1:5000/update/${id}/${novaTarefa}`;
+        axios.put(url)
+            .then(resposta => {
+                console.log("Tarefa editada com sucesso:", resposta.data);
+            })
+            .catch(erro => console.error("Erro ao editar a tarefa:", erro));
+    }
+
+}
